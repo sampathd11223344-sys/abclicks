61711bec8d01b362ec650637b5d7ba4789b5ac0c
sampathd11223344-sys <sampathd11223344@gmail.com>
2026-04-03 18:35:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,47 @@ function App() {
     <div style={{
       background: "black",
       color: "white",
-      height: "100vh",
+      minHeight: "100vh",
       display: "flex",
       flexDirection: "column",
       justifyContent: "center",
       alignItems: "center",
-      textAlign: "center"
+      textAlign: "center",
+      fontFamily: "sans-serif"
     }}>
-      <h1>AB Clicks</h1>
-      <p>Capture Moments Create Memories</p>
+      
+      <h1 style={{ 
+        fontSize: "60px", 
+        lineHeight: "1.2",
+        marginBottom: "20px"
+      }}>
+        Capture Moments <br />
+        Create Memories
+      </h1>
+
+      <p style={{ 
+        color: "#aaa", 
+        marginBottom: "30px" 
+      }}>
+        Cinematic Photography & Videography
+      </p>
+
+      <a 
+        href="https://wa.me/918919852330"
+        target="_blank"
+        rel="noreferrer"
+        style={{
+          padding: "14px 35px",
+          background: "white",
+          color: "black",
+          borderRadius: "30px",
+          textDecoration: "none",
+          fontWeight: "bold"
+        }}
+      >
+        Book Now
+      </a>
+
     </div>
   );
 }
